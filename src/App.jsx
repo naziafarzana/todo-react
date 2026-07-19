05e05dd0e545555e932dae54bdc300605ff68e4e
naziafarzana <231574097+naziafarzana@users.noreply.github.com>
2026-07-19 15:13:37 +0600

    Build functional SubmitBtn & input text field with styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
 
-const App = () => {
+
+
+
+
+
+ const App = () => {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (text) => {
+    const newTodo = { id: Date.now(), text: text}
+    setTodos([...todos, newTodo])
+  }
+
   return (
     <div>
-      <h1 className='font-bold text-4xl '>Hello</h1>
+      <h1 className='font-bold text-4xl'>Hello</h1>
+      <TodoInput onAdd={addTodo} />
+      <TodoList todos={todos} />
     </div>
   )
 }
+
 
 export default App
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
 import { useState } from 'react'
 
-const TodoInput = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+// export default function TodoInput() {
+//   return (
+//     <div>
+//       <input value={"some text"} />
+//       <button>Submit</button>
+//     </div>
+//   )
+// }
 
 function TodoInput({ onAdd }) {
     const [text, setText] = useState("")
-    
 
-    values = {text} 
-    onChange = {(e)=>setText(e.targer.value)}
-    <button onClick = {handleSubmit}>SubmitBtn</button>
+    const handleSubmit = () => {
+        onAdd(text)
+        setText('')
+        // values = {text} 
+        // onChange = {(e)=>setText(e.targer.value)}
+        // <button onClick = {handleSubmit}>SubmitBtn</button>
+    }
 
-
-
-
-
+    return (
+        <div>
+      <input value={text} onChange={(e) => setText(e.target.value)} className='border-black border-2 rounded' />
+      <button onClick={handleSubmit} className='bg-blue-500 text-white px-4 py-1 rounded'>SubmitBtn</button>
+    </div>
+    )
 
 }
 export default TodoInput
+
+
+
+
+
+
+
